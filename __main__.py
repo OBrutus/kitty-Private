7f06e1ee667c38c94d9214018a6cf9c4fc987010
VaishnaV <aniket.vaishnav@bito.ai>
2024-11-08 16:01:12 +0530

    small changes

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # License: GPL v3 Copyright: 2015, Kovid Goyal <kovid at kovidgoyal.net>
 
+# just a random commit
 
 if __name__ == '__main__':
     from kitty.entry_points import main
